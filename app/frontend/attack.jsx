2d718e4b9d2b742215b8e67143dde829a19daae2
Taran Cacacho <tarancacacho@gmail.com>
2018-12-11 15:15:46 -0800

    Import ReactDOM from correct module

--- app/frontend/attack.jsx
+++ app/frontend/attack.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root")
+  const root = document.getElementById("root");
   ReactDOM.render(<h1>Where Saving the World Happens</h1>, root)
 });
