ea08eaf985d6d4f908636d3ecbdcec512549a696
Taran Cacacho <tarancacacho@gmail.com>
2018-12-11 14:30:13 -0800

    Render header into root element

--- app/frontend/attack.jsx
+++ app/frontend/attack.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root")
-  ReactDOM.render(<h1>)
-})
+  ReactDOM.render(<h1>Where Saving the World Happens</h1>, root)
+});
