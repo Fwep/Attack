a02e0033e90a9bc4159a6ad846805ee21a0176f9
Taran Cacacho <tarancacacho@gmail.com>
2018-12-12 15:33:27 -0800

    Fix typo

--- app/frontend/reducers/root_reducer.js
+++ app/frontend/reducers/root_reducer.js
@@ -5,7 +5,7 @@ import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 
 const rootReducer = combineReducers({
-  entitites: entitiesReducer,
+  entities: entitiesReducer,
   session: sessionReducer,
   errors: errorsReducer
 })
