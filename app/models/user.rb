6bc81e7724cc66b76604390aa01a5ed2275b8172
Taran Cacacho <tarancacacho@gmail.com>
2018-12-12 08:57:01 -0800

    Add user auth methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,24 @@ class User < ApplicationRecord
   validates :username, :email, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
 
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -46,5 +64,9 @@ class User < ApplicationRecord
   private
   def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 end
