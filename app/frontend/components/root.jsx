7114a7965fdd3b0efcbbf2d1735ef9909d1a29b4
Taran Cacacho <tarancacacho@gmail.com>
2018-12-12 13:34:32 -0800

    Wrap App in HashRouter

--- app/frontend/components/root.jsx
+++ app/frontend/components/root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Route, HashRouter } from 'react-router-dom';
+import App from './App';
 
-const Root = ({ store }) => {
+const Root = ({ store }) => (
   <Provider store={store}>
-  // Router goes here...
+    <HashRouter>
+      <App/>
+    </HashRouter>
   </Provider>
-}
+);
 
 export default Root;
