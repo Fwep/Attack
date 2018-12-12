23eac80088f5a04b6b221e06decdf1935fed4739
Taran Cacacho <tarancacacho@gmail.com>
2018-12-11 17:39:53 -0800

    Write Api::Sessions#create controller action

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,18 @@
 class Api::SessionsController < ApplicationController
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      render json: 'Credentials are wrong'
+    else
+      render json: "Welcome back #{user.username}!"
+    end
+  end
+
+  def new
+
+  end
 end
