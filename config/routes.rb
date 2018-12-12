b07be7b7ec26c4d979ae05848bd1f6bdb9480d90
Taran Cacacho <tarancacacho@gmail.com>
2018-12-12 09:41:35 -0800

    Fix typo (users was singularized)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
-    resource :user, only: [:create, :destroy]
+    resource :users, only: [:create, :destroy]
   end
 end
