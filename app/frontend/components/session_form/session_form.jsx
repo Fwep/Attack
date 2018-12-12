7c940734747b2f2b9a6665ac331635ca1734161f
Taran Cacacho <tarancacacho@gmail.com>
2018-12-12 15:32:52 -0800

    Write out session form presentational component

---
+++ app/frontend/components/session_form/session_form.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+  this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    e => {
+      this.setState({[field]: e.target.value})
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user)
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, idx) => (
+          <li key={`error-${idx}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <br/>
+            Please {this.props.formType} or {this.props.navLink}
+            {this.renderErrors()}
+            <div className="login-form">
+              <br/>
+
+              <label>Username:
+                <input type="text"
+                  value={this.state.username}
+                  onChange={this.update('username')}
+                  className="login-input"
+                />
+              </label>
+
+              <br/>
+
+              <label>Password:
+                <input type="password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  className="login-input"
+                />
+              </label>
+
+              <br/>
+
+              <input type="submit" value={this.props.formType} />
+            </div>
+
+          </form>
+        </div>
+      );
+    }
+}
+
+export default SessionForm;
