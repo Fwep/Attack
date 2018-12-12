2430187c2685b9cd05d4a35572e6e3cd261b4a75
Taran Cacacho <tarancacacho@gmail.com>
2018-12-12 11:51:05 -0800

    Create Root component

---
+++ app/frontend/components/root.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+const Root = ({ store }) => {
+  <Provider store={store}>
+  // Router goes here...
+  </Provider>
+}
+
+export default Root;
