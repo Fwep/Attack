92aa164524044e8be241adaf5caa93dc39e5fe0a
Taran Cacacho <tarancacacho@gmail.com>
2018-12-12 09:43:23 -0800

    Write user new and user creation methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,21 @@
 class Api::UsersController < ApplicationController
   before_action :require_current_user!, except: [:create, :new]
+  def create
+    @user = User.new(user_params)
+    debugger
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
 end
