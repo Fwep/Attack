cba947427c0dcb4e3de342d2caf3c26eef7311b4
Taran Cacacho <tarancacacho@gmail.com>
2018-12-13 12:26:40 -0800

    Add Slack blurb and image

---
+++ app/frontend/components/greeting/greeting_body.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const GreetingBody = () => (
+  <div className="greeting_body">
+    <img className="cooler" src="/assets/batman_and_superman_water_cooler.jpg"/>
+    <p className="greeting-blurb">
+      When your team needs to kick butt, hire a new English butler,
+      deploy some AI super-assistant, review a battle plan, finalize
+      next year's reparations budget, measure a mutant's vitals,
+      or simply plan your next office meeting, and more, Attack has
+      you covered.
+    </p>
+
+  </div>
+);
+
+export default GreetingBody;
