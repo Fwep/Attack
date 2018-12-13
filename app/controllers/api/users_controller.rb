1f8f81ce7855e2773eb114d6d0fb75e9e76b114d
Taran Cacacho <tarancacacho@gmail.com>
2018-12-12 22:15:17 -0800

    Render show view instead of json:@user upon save

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,9 +2,10 @@ class Api::UsersController < ApplicationController
   before_action :require_current_user!, except: [:create, :new]
   def create
     @user = User.new(user_params)
+
     if @user.save
       login!(@user)
-      render json: @user
+      render json "api/users/show"
     else
       render json: @user.errors.full_messages
     end
