228f0207ed6752c46fe2db6fd7e9f2d2ef68494e
Taran Cacacho <tarancacacho@gmail.com>
2018-12-14 12:40:38 -0800

    Remove blurb and signin div

--- app/frontend/components/greeting/greeting_body.jsx
+++ app/frontend/components/greeting/greeting_body.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-// <div>
-//   <h2>The hub for your team and your work</h2>
-//   <p className="greeting-blurb">
-//     Attack is a place where your team comes together to collaborate,
-//     important information can be found by the right people, and your
-//     tools pipe in information when and where you need it.
-//   </p>
-// </div>
-
-const GreetingBody = () => (
+const GreetingBody = ({currentUser, logout}) => (
   <div className="main-greeting-container">
     <img className="cooler hero_content"
     src="/assets/batman_and_superman_water_cooler.jpg"
@@ -27,11 +17,6 @@ const GreetingBody = () => (
         you covered.
       </p>
     </div>
-
-    <div className="signin">
-      <p>dsafsdf</p>
-    </div>
-
   </div>
 );
 
