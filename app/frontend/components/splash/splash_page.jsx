c6f6f7cd218b0d25658a3e250a1906dd2d9b0fa6
Taran Cacacho <tarancacacho@gmail.com>
2018-12-14 12:38:25 -0800

    Add file

---
+++ app/frontend/components/splash/splash_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GreetingContainer from '../greeting/greeting_container';
+import SignUpFormContainer from '../session_form/signup_form_container';
+
+const SplashPage = () => (
+  <div>
+    <GreetingContainer/>
+  </div>
+);
+
+export default SplashPage;
