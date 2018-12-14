48b2e18a45b9becc5d4c63769e22d5121b683f9b
Taran Cacacho <tarancacacho@gmail.com>
2018-12-13 16:35:43 -0800

    Write out splash page blurbs and headers

--- app/frontend/components/greeting/greeting_body.jsx
+++ app/frontend/components/greeting/greeting_body.jsx
@@ -3,13 +3,24 @@ import { Link } from 'react-router-dom';
 
 const GreetingBody = () => (
   <div className="greeting_body">
-    <img className="cooler" src="/assets/batman_and_superman_water_cooler.jpg"/>
+    <h1 id="headline">Where Saving the World Happens</h1>
+    <img className="cooler"
+    src="/assets/batman_and_superman_water_cooler.jpg"
+    alt="Attack brings all your communication together"
+    />
     <p className="greeting-blurb">
       When your team needs to kick butt, hire a new English butler,
       deploy some AI super-assistant, review a battle plan, finalize
       next year's reparations budget, measure a mutant's vitals,
       or simply plan your next office meeting, and more, Attack has
       you covered.
+    </p>
+
+    <h2>The hub for your team and your work</h2>
+    <p className="greeting-blurb">
+    Attack is a place where your team comes together to collaborate,
+    important information can be found by the right people, and your
+    tools pipe in information when and where you need it.
     </p>
 
   </div>
