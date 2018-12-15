795f594e437ea57258094c2c5882f868df2c3ef1
Taran Cacacho <tarancacacho@gmail.com>
2018-12-14 20:59:50 -0800

    Add splash image, headline, and rename element class names

--- app/frontend/components/greeting/greeting_body.jsx
+++ app/frontend/components/greeting/greeting_body.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const GreetingBody = ({currentUser, logout}) => (
-  <div className="main-greeting-container">
-    <img className="cooler hero_content"
-    src="/assets/batman_and_superman_water_cooler.jpg"
-    alt="Attack brings all your communication together"
-    />
-
-    <div className="greeting_body hero_content">
-      <h1 className="greeting-header">Where Saving the World Happens</h1>
-      <p className="greeting-blurb">
+  <div className="splash">
+    <header className="splash__header">
+      <h1 className="splash__header__headline">Where Saving the World Happens</h1>
+      <p className="splash__header__blurb">
         When your team needs to kick butt, hire a new English butler,
         deploy an AI super-assistant, review a battle plan, finalize
         next year's reparations budget, measure a mutant's vitals,
         or simply plan your next office meeting, and more, Attack has
         you covered.
       </p>
+    </header>
+
+    <div className="splash__image--main">
+      <img src="/assets/batman_and_superman_water_cooler.jpg"
+      alt="Attack brings all your communication together"/>
     </div>
   </div>
 );
