f439d7a5bd0bb84498530c21f90711ca262c9f33
Taran Cacacho <tarancacacho@gmail.com>
2018-12-14 20:42:14 -0800

    Create functional splash container component

---
+++ app/frontend/components/splash/splash.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GreetingContainer from '../greeting/greeting_container';
+import SignUpFormContainer from '../session_form/signup_form_container';
+
+const Splash = () => (
+  <div>
+    <
+    <GreetingContainer/>
+  </div>
+);
+
+export default Splash;
