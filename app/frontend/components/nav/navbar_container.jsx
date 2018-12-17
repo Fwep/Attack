d35b0d6db763a668381dcfa5e6688a7a08d10a61
Taran Cacacho <tarancacacho@gmail.com>
2018-12-16 20:53:42 -0800

    Add navbar container component

---
+++ app/frontend/components/nav/navbar_container.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import NavBar from './navbar';
+
+const mapStateToProps = state => ({
+  currentUser: state.session.id,
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logout: () => dispatch(logout())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
