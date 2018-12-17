6cf9a766edba993bde99c1a22a36c4426018f914
Taran Cacacho <tarancacacho@gmail.com>
2018-12-16 20:59:26 -0800

    Write structure of navbar--plain

---
+++ app/frontend/components/nav/navbar--plain.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PlainNavBar = ({history}) => (
+  <div className="navbar navbar--plain">
+    <ul className="navbar__copy-block">
+      <li className="logo">
+        <img onClick={() => this.props.history.push('/')} src="/assets/attackfavicon.png"
+        alt="attackfavicon"/>
+      </li>
+    </ul>
+  </div>
+);
+
+export default PlainNavBar;
