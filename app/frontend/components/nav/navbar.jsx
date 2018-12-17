e82428c58fc4d0d103b7f4c504c61d4428d248f3
Taran Cacacho <tarancacacho@gmail.com>
2018-12-16 20:57:36 -0800

    Conditionally render signin/signup buttons based on logged_in status

--- app/frontend/components/nav/navbar.jsx
+++ app/frontend/components/nav/navbar.jsx
@@ -1,41 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = () => (
-  <div className="navbar">
-    <ul className="navbar__copy-block">
-      <li className="logo">
-        <img src="/assets/attackfavicon.png"
-        alt="attackfavicon"/>
-      </li>
-      <li>Why Attack?</li>
-      <li>Solutions</li>
-      <li><a href="https://www.github.com/Fwep/Attack.git">Resources</a></li>
-      <li>Pricing</li>
-    </ul>
-
-    <ul className="hamburger-dropdown">
+const NavBar = ({currentUser, logout}) => {
+  const rightDisplay = currentUser ?  (
+    <div>
+      <p>Hello, {currentUser.username}</p>
+      <button onClick={logout}>Log out</button>
+    </div>
+  ) : (
+    <div>
+      <ul className="hamburger-dropdown">
       <li>
-        <ul className="navbar__form-block hamburger__ul">
-          <li className="form__list-el form__list-el--sign-up">
-            <Link to="/signin">Sign in</Link>
-          </li>
-          <li className="form__list-el form__list-el--sign-up">
-            <Link to="/signup" className="form__sign-up ">Get Started</Link>
-          </li>
-        </ul>
+      <ul className="navbar__form-block hamburger__ul">
+      <li className="form__list-el form__list-el--sign-up">
+      <Link to="/signin">Sign in</Link>
       </li>
-    </ul>
+      <li className="form__list-el form__list-el--sign-up">
+      <Link to="/signup" className="form__sign-up ">Get Started</Link>
+      </li>
+      </ul>
+      </li>
+      </ul>
 
-    <ul className="navbar__form-block">
+      <ul className="navbar__form-block">
       <li className="form__list-el form__list-el--sign-up">
-        <Link to="/signin">Sign in</Link>
+      <Link id="thisisitchief" to="/signin">Sign in</Link>
       </li>
       <li className="form__list-el form__list-el--sign-up">
-        <Link to="/signup" className="form__sign-up ">Get Started</Link>
+      <Link to="/signup" className="form__sign-up ">Get Started</Link>
       </li>
-    </ul>
-  </div>
-);
+      </ul>
+    </div>
+  )
+
+  return (
+    <div className="navbar">
+      <ul className="navbar__copy-block">
+        <li className="logo">
+          <img src="/assets/attackfavicon.png"
+          alt="attackfavicon"/>
+        </li>
+        <li>Why Attack?</li>
+        <li>Solutions</li>
+        <li><a href="https://www.github.com/Fwep/Attack.git">Resources</a></li>
+        <li>Pricing</li>
+      </ul>
+      {rightDisplay}
+    </div>
+  );
+
+};
 
 export default NavBar;
