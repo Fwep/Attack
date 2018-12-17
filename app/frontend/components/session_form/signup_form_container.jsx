adbe304fd36d92283fd935e9268f8fd44ab2ecd6
Taran Cacacho <tarancacacho@gmail.com>
2018-12-17 14:48:03 -0800

    Add clear method to mDP

--- app/frontend/components/session_form/signup_form_container.jsx
+++ app/frontend/components/session_form/signup_form_container.jsx
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { signup } from '../../actions/session_actions';
+import { signup, clearErrors } from '../../actions/session_actions';
 import SignUpForm from './signup_form';
 
 const mapStateToProps = state => {
@@ -9,7 +9,8 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => ({
-  processForm: formUser => dispatch(signup(formUser))
+  processForm: formUser => dispatch(signup(formUser)),
+  clear: () => dispatch(clearErrors())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignUpForm);
