fd11ddfd3f7e2e24e3c3e45287b1b70db3f170be
Taran Cacacho <tarancacacho@gmail.com>
2018-12-17 14:44:55 -0800

    Remove redundant json flag on render, add status code to user error rendering

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,9 +5,9 @@ class Api::UsersController < ApplicationController
 
     if @user.save
       login!(@user)
-      render json "api/users/show"
+      render "api/users/show"
     else
-      render json: @user.errors.full_messages
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
