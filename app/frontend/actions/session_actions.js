bc6806760399a49a5712e8e399704378801844a1
Taran Cacacho <tarancacacho@gmail.com>
2018-12-17 14:57:29 -0800

    Refactor for readability

--- app/frontend/actions/session_actions.js
+++ app/frontend/actions/session_actions.js
@@ -3,6 +3,7 @@ import * as APIUtil from '../util/session_api_util';
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
 
 export const receiveCurrentUser = user => {
   return {
@@ -24,26 +25,25 @@ export const receiveErrors = errors => {
   }
 }
 
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
+});
+
 export const login = user => dispatch => {
   return APIUtil.login(user)
-    .then(user => (
-      dispatch(receiveCurrentUser(user))
-    , err => (
-      dispatch(receiveErrors(err.responseJSON))
-    )))
+    .then(user => dispatch(receiveCurrentUser(user)),
+      err => dispatch(receiveErrors(err.responseJSON)))
 };
 
 export const logout = () => dispatch => {
   return APIUtil.logout()
-    .then(() => (
+    .then(user => (
       dispatch(logoutCurrentUser())
-    , err => (
-      dispatch(receiveErrors(err.responseJSON))
-    )))
+    ))
 };
 
 export const signup = user => dispatch => {
   return APIUtil.signup(user)
-    .then(user => (
-      dispatch(receiveCurrentUser(user))))
+    .then(user => dispatch(receiveCurrentUser(user)),
+      err => dispatch(receiveErrors(err.responseJSON)));
 };
