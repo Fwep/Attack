6545937e5320b331f414ad38212c063e5b9c1e4c
Taran Cacacho <tarancacacho@gmail.com>
2018-12-17 14:44:05 -0800

    Import merge function

--- app/frontend/reducers/users_reducer.js
+++ app/frontend/reducers/users_reducer.js
@@ -2,6 +2,8 @@ import {
   RECEIVE_CURRENT_USER
 } from '../actions/session_actions';
 
+import merge from 'lodash/merge';
+
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
