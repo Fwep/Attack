8b8b66f54e7778aaa85c58f0afddce135393ff2a
Taran Cacacho <tarancacacho@gmail.com>
2018-12-16 17:02:07 -0800

    Add notes for clarity

--- app/frontend/util/session_api_util.js
+++ app/frontend/util/session_api_util.js
@@ -1,4 +1,4 @@
-export const signup = ({user}) => {
+export const signup = ({user}) => { // AJAX request to create a new user
   return $.ajax({
     method: 'POST',
     url: 'api/users',
@@ -6,7 +6,7 @@ export const signup = ({user}) => {
   });
 };
 
-export const login = (user) => {
+export const login = (user) => { // AJAX request to create a new session (login)
   return $.ajax({
     method: 'POST',
     url: 'api/session',
@@ -14,7 +14,7 @@ export const login = (user) => {
   });
 };
 
-export const logout = () => {
+export const logout = () => { // AJAX request to delete a new session (logout)
   return $.ajax({
     method: 'DELETE',
     url: 'api/session'
