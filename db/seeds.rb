20dd59e25ff09b278b8be4820ae6749307c42160
Taran Cacacho <tarancacacho@gmail.com>
2018-12-17 16:34:20 -0800

    WET up seeds (adding a subdirectory didn't work)

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create(username: 'Superman', email: 'clark.kent@dailyplanet.io', password: 'ILoveLois123')
+User.create(username: 'Batman', email: 'billionaire_playboy_philanthropist@wayneindustries.co', password: 'TonyStarkSucks')
+User.create(username: 'Spiderman', email: 'pp2002@yahoo.com', password: 'gr3@tpowergr3@tresp0ns!b1l1ty')
