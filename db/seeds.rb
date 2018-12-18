8c6a13f1e7743ab4c5b989e6cb8f3b617fe6e329
Taran Cacacho <tarancacacho@gmail.com>
2018-12-17 17:17:13 -0800

    Populate with initial users

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ User.destroy_all
 User.create(username: 'Superman', email: 'clark.kent@dailyplanet.io', password: 'ILoveLois123')
 User.create(username: 'Batman', email: 'billionaire_playboy_philanthropist@wayneindustries.co', password: 'TonyStarkSucks')
 User.create(username: 'Spiderman', email: 'pp2002@yahoo.com', password: 'gr3@tpowergr3@tresp0ns!b1l1ty')
+User.create(username: 'TheRealIronman', email: 'anthonystark@mit.edu', password: 'Pepper')
+User.create(username: 'Deadpool', email: 'wadewilson@gmail.com', password: 'ball$')
+User.create(username: 'KyloRen', email: 'notahero@empire.first', password: 'sassofrass')
