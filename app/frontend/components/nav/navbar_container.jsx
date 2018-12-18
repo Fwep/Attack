cdbc5c2fc9d5ddf7f10d8b170b27b815380c1b1f
Taran Cacacho <tarancacacho@gmail.com>
2018-12-17 17:34:17 -0800

    Fix destructuring of state to extract current user

--- app/frontend/components/nav/navbar_container.jsx
+++ app/frontend/components/nav/navbar_container.jsx
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import NavBar from './navbar';
 
-const mapStateToProps = state => ({
-  currentUser: state.session.id,
+const mapStateToProps = (state) => ({
+  currentUser: state.entities.users[state.session.id]
 })
 
 const mapDispatchToProps = dispatch => {
