06894eea861181ab89a3933bdee558403691ad24
Taran Cacacho <tarancacacho@gmail.com>
2018-12-19 15:43:24 -0800

    Add route for messages index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
     resource :users, only: [:create, :destroy]
+    resources :messages, only: [:index]
   end
 end
