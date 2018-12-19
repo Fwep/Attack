4b26c2618fb39af17d49c41992f83fa39066ea64
Taran Cacacho <tarancacacho@gmail.com>
2018-12-19 15:35:13 -0800

    Generate Messages model

--- db/migrate/20181219232701_create_messages.rb
+++ db/migrate/20181219232701_create_messages.rb
@@ -2,7 +2,6 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.references :user, foreign_key: true
-      t.references :channel, foreign_key: true
       t.text :body, null: false
 
       t.timestamps
