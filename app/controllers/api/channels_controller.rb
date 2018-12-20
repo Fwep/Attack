3a87a8ad8060bc4e6862005859452173750c7e2e
Taran Cacacho <tarancacacho@gmail.com>
2018-12-19 21:05:56 -0800

    Write CRU actions for Channels controller

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,2 +1,34 @@
 class Api::ChannelsController < ApplicationController
+  def show
+    @channel = Channel.find_by(params[:id])
+    render "api/channels/show" if @channel
+  end
+
+  def index
+    @channels = Channel.all
+    render "api/views/index"
+  end
+
+  def create
+    @channel = Channel.new(channel_params)
+    if @channel.save
+      render "api/channels/show"
+    else
+      render @channel.errors.full_messages
+    end
+  end
+
+  def update
+    @channel = Channel.new(channel_params)
+    if @channel.save
+      render "api/channels/show"
+    else
+      render @channel.errors.full_messages
+    end
+  end
+
+  private
+  def channel_params
+    params.require(:channel).permit(:creator_id, :name, :description, :private, :is_direct)
+  end
 end
