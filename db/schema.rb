cf3d7578a62ad8e3ee4a402c3136fb79531b65db
Taran Cacacho <tarancacacho@gmail.com>
2018-12-19 17:30:47 -0800

    Generate Subscriptions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_011435) do
+ActiveRecord::Schema.define(version: 2018_12_20_012530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2018_12_20_011435) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "channel_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id"], name: "index_subscriptions_on_channel_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
@@ -48,4 +57,6 @@ ActiveRecord::Schema.define(version: 2018_12_20_011435) do
   end
 
   add_foreign_key "messages", "users"
+  add_foreign_key "subscriptions", "channels"
+  add_foreign_key "subscriptions", "users"
 end
