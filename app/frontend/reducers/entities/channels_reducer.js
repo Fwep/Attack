981368289e25c596552e6c65dcb49da84ea99abf
Taran Cacacho <tarancacacho@gmail.com>
2018-12-20 11:23:52 -0800

    Write channelsReducer

--- app/frontend/reducers/entities/channels_reducer.js
+++ app/frontend/reducers/entities/channels_reducer.js
@@ -4,12 +4,28 @@ import {
 } from '../../actions/channel_actions';
 import merge from 'lodash/merge';
 
-const channelsReducer = (state = {}, action) => {
-  Object.freeze(state);
+const = _nullChannel = {
+  1: {
+    id: 1,
+    creator_id: 1,
+    title: 'nullChannel',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    private: false,
+    is_direct: false,
+  }
+}
 
+const channelsReducer = (state = _nullChannel, action) => {
+  Object.freeze(state);
+    let newState = {};
   switch (action.type) {
     case RECEIVE_CHANNEL:
-      return merge()
+      return merge({}, state, {[action.channel.id]: action.channel})
+    case RECEIVE_CHANNELS:
+      action.channels.forEach(channel => {
+        newState[channel.id] = channel
+      })
+      return newState;
     default:
       return state;
   }
