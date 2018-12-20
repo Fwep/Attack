0caab6e9d43535a8103b4a055326036a2843669e
Taran Cacacho <tarancacacho@gmail.com>
2018-12-19 17:46:44 -0800

    Annotate models

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  channel_id :bigint(8)
+#
+
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :channel
--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: channels
+#
+#  id          :bigint(8)        not null, primary key
+#  creator_id  :integer          not null
+#  name        :string           not null
+#  description :text
+#  private     :boolean          default(FALSE), not null
+#  is_direct   :boolean          default(FALSE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ChannelTest < ActiveSupport::TestCase
