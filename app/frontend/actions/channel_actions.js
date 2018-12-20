8f27c4d34fe6cc44925c1c09639cdc13060c1d29
Taran Cacacho <tarancacacho@gmail.com>
2018-12-20 11:22:46 -0800

    Add thunk action creators and normal action creators

---
+++ app/frontend/actions/channel_actions.js
@@ -0,0 +1,38 @@
+import * as ChannelAPIUtil from '../util/channels_api_util';
+
+export const RECEIVE_CHANNEL = "RECEIVE_CHANNEL";
+export const RECEIVE_CHANNELS = "RECEIVE_ALL_CHANNELS";
+
+export const receiveChannel = channel => {
+  return {
+    type: RECEIVE_CHANNEL,
+    channel
+  }
+}
+
+export const receiveChannels = channels => {
+  return {
+    type: RECEIVE_CHANNELS,
+    channels
+  }
+}
+
+export const createChannel = channel => dispatch => {
+  return ChannelAPIUtil.createChannel(channel)
+    .then(channel => dispatch(receiveChannel(channel))
+};
+
+export const fetchChannel = id => dispatch => {
+  return ChannelAPIUtil.fetchChannel(id)
+    .then(channel => dispatch(receiveChannel(channel)))
+};
+
+export const fetchChannels = () => dispatch => {
+  return ChannelAPIUtil.fetchChannels()
+    .then(channels => dispatch(receiveChannels(channels)))
+};
+
+export const updateChannel = (channel) => dispatch => {
+  return ChannelAPIUtil.updateChannel()
+    .then(channel => dispatch(receiveChannel(channel)))
+};
