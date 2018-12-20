94a2aa0cf40d89d21634c13487669afae2224d2d
Taran Cacacho <tarancacacho@gmail.com>
2018-12-20 15:41:02 -0800

    Set default state to empty POJO

--- app/frontend/reducers/entities/channels_reducer.js
+++ app/frontend/reducers/entities/channels_reducer.js
@@ -4,27 +4,15 @@ import {
 } from '../../actions/channel_actions';
 import merge from 'lodash/merge';
 
-const = _nullChannel = {
-  1: {
-    id: 1,
-    creator_id: 1,
-    title: 'nullChannel',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-    private: false,
-    is_direct: false,
-  }
-}
 
-const channelsReducer = (state = _nullChannel, action) => {
+const channelsReducer = (state = {}, action) => {
   Object.freeze(state);
     let newState = {};
   switch (action.type) {
     case RECEIVE_CHANNEL:
       return merge({}, state, {[action.channel.id]: action.channel})
     case RECEIVE_CHANNELS:
-      action.channels.forEach(channel => {
-        newState[channel.id] = channel
-      })
+      return merge({}, state, action.channels);
       return newState;
     default:
       return state;
