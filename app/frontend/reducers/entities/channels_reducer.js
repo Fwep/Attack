b85d37a66336ac45b335fbc4cd4118fe1b377efc
Taran Cacacho <tarancacacho@gmail.com>
2018-12-21 13:50:08 -0800

    Return newState empty object

--- app/frontend/reducers/entities/channels_reducer.js
+++ app/frontend/reducers/entities/channels_reducer.js
@@ -7,13 +7,11 @@ import merge from 'lodash/merge';
 
 const channelsReducer = (state = {}, action) => {
   Object.freeze(state);
-    let newState = {};
   switch (action.type) {
     case RECEIVE_CHANNEL:
       return merge({}, state, {[action.channel.id]: action.channel})
     case RECEIVE_CHANNELS:
       return merge({}, state, action.channels);
-      return newState;
     default:
       return state;
   }
