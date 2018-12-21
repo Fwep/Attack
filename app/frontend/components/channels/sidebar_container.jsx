620c1aa7e76333a408cd6918afa9342fb101d232
Taran Cacacho <tarancacacho@gmail.com>
2018-12-21 13:51:32 -0800

    Add currentUser, channels, and fetchChannels action creator as props to Sidebar

--- app/frontend/components/channels/sidebar_container.jsx
+++ app/frontend/components/channels/sidebar_container.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchChannels } from '../../actions/channel_actions';
+import userChannels from '../../reducers/selectors';
+import Sidebar from './sidebar';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-
+    currentUser: state.entities.users[state.session.id],
+    channels: userChannels(state)
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-
+    fetchChannels: () => dispatch(fetchChannels())
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)()
-
-export default SidebarContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
