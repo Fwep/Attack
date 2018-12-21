633ae7d74f5885742b5a4d8d98ed017492438a12
Taran Cacacho <tarancacacho@gmail.com>
2018-12-21 13:48:25 -0800

    Add RECEIVE_CHANNELS type to users reducer

--- app/frontend/reducers/entities/users_reducer.js
+++ app/frontend/reducers/entities/users_reducer.js
@@ -1,12 +1,17 @@
 import {
   RECEIVE_CURRENT_USER
 } from '../../actions/session_actions';
+import {
+  RECEIVE_CHANNELS
+} from '../../actions/channel_actions';
 
 import merge from 'lodash/merge';
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_CHANNELS:
+      return merge({}, state, action.users);
     case RECEIVE_CURRENT_USER:
       return merge({}, state, {[action.user.id]: action.user});
     default:
