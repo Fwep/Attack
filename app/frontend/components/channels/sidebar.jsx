97a438c89b8645a4c0157f3fefd0d81bb7745d07
Taran Cacacho <tarancacacho@gmail.com>
2018-12-20 17:25:32 -0800

    Add file

---
+++ app/frontend/components/channels/sidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    const channels = (
+      <ol>
+
+      </ol>
+    )
+
+    return(
+      <div className="sidebar">
+        <h1>SIDEBAR</h1>
+      </div>
+    );
+  };
+};
+
+export default Sidebar;
