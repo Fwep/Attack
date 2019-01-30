b8b00213941d17ec3af5b6d1edd3fda309d77334
Taran Cacacho <tarancacacho@gmail.com>
2019-01-30 00:39:58 -0800

    Add create and message_params methods

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,11 @@
 class Api::MessagesController < ApplicationController
+  def create
+    @message = Message.new(message_params)
+    
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:user_id, :channel_id, :body)
+  end
 end
