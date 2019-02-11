6df12fbe2131f49c9e37a061c0b4c3b7b33eeb15
Fwep <tarancacacho@gmail.com>
2019-02-10 23:16:17 -0800

    Add functionality to pull all channels from Rails API

--- frontend/actions/channel_actions.js
+++ frontend/actions/channel_actions.js
@@ -1,40 +1,13 @@
 import * as ChannelAPIUtil from '../util/channels_api_util';
 
-export const RECEIVE_CHANNEL = "RECEIVE_CHANNEL";
-export const RECEIVE_CHANNELS = "RECEIVE_ALL_CHANNELS";
+export const RECEIVE_CHANNELS = "RECEIVE_CHANNELS";
 
-export const receiveChannel = channel => {
-  return {
-    type: RECEIVE_CHANNEL,
-    channel
-  }
-}
+export const receiveChannels = channels => ({
+  type: RECEIVE_CHANNELS,
+  channels
+});
 
-export const receiveChannels = payload => {
-  return {
-    type: RECEIVE_CHANNELS,
-    channels: payload.channels,
-    subscriptions: payload.subscriptions,
-    users: payload.users,
-  }
-}
-
-export const createChannel = channel => dispatch => {
-  return ChannelAPIUtil.createChannel(channel)
-    .then(channel => dispatch(receiveChannel(channel)))
-};
-
-export const fetchChannel = id => dispatch => {
-  return ChannelAPIUtil.fetchChannel(id)
-    .then(channel => dispatch(receiveChannel(channel)))
-};
-
-export const fetchChannels = () => dispatch => {
-  return ChannelAPIUtil.fetchChannels()
-    .then(payload => dispatch(receiveChannels(payload)))
-};
-
-export const updateChannel = (channel) => dispatch => {
-  return ChannelAPIUtil.updateChannel(channel)
-    .then(channel => dispatch(receiveChannel(channel)))
-};
+export const fetchChannels = () => dispatch => (
+  ChannelAPIUtil.fetchChannels()
+    .then(channels => dispatch(receiveChannels(channels)))
+);
