260083c168c7d0b940c9c191817cb0ef55b28578
Fwep <tarancacacho@gmail.com>
2019-02-11 18:07:13 -0800

    Comment out message broadcasting

--- app/channels/channel_channel.rb
+++ app/channels/channel_channel.rb
@@ -1,17 +1,16 @@
 # We made this (I named it perfectly, calling it 'Channel')
-
 class ChannelChannel < ApplicationCable::Channel
   def subscribed
-    stream_for "channel_channel"
+    stream_from "channel_channel"
   end
 
   def speak(data)
-    message = Message.create(body: data['message'])
+    message = Message.create(data['message'])
 
-    # This is an object. Broadcasting a string will throw an error
-    socket = { message: message.body } 
+    # # This is an object. Broadcasting a string will throw an error
+    # socket = { message: message.body } 
     
-    ChannelChannel.broadcast_to('channel_channel', socket)
+    # ChannelChannel.broadcast_to('channel_channel', socket)
   end
 
   def unsubscribed
