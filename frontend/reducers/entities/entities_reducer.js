bae954c59f03c18d006643916c563ffbfb80a0ee
Fwep <tarancacacho@gmail.com>
2019-02-11 18:10:15 -0800

    Add messages slice of state

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import usersReducer from './users_reducer';
 import channelsReducer from './channels_reducer';
+import messagesReducer from './messages_reducer';
 import subscriptionsReducer from './subscriptions_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   channels: channelsReducer,
+  messages: messagesReducer,
   subscriptions: subscriptionsReducer
 });
 
