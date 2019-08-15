f4c060733709881452d83c070f25aedeef002221
Taran Cacacho <tarancacacho@gmail.com>
2019-08-14 23:59:58 -0700

    Change channel column 'private' to 'is_private'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_013602) do
+ActiveRecord::Schema.define(version: 2019_08_15_021537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2018_12_20_013602) do
     t.integer "creator_id", null: false
     t.string "name", null: false
     t.text "description"
-    t.boolean "private", default: false, null: false
+    t.boolean "is_private", default: false, null: false
     t.boolean "is_direct", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
