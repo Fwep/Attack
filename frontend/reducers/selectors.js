760d1b5b98ab6e804d01610946485648a29a9f01
Taran Cacacho <tarancacacho@gmail.com>
2019-08-15 15:17:15 -0700

    Remove extraneous uppercasing of channel names and create selector to return array-formatted channel messages

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,7 +1,11 @@
 export const getUsersChannels = ({ entities: { channels } }) => (
   Object.values(channels).sort((ch1, ch2) => {
-    const nameA = ch1.name.toUpperCase();
-    const nameB = ch2.name.toUpperCase();
+    const nameA = ch1.name;
+    const nameB = ch2.name;
     return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
   })
+);
+
+export const getChannelMessages = ({ entities: { messages } }) => (
+  Object.values(messages).length ? Object.values(messages) : []
 );
