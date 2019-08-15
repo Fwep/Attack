3002729a6c4e13aea00d395f6bcb7392c6e8edb3
Taran Cacacho <tarancacacho@gmail.com>
2019-08-14 23:59:34 -0700

    Add API functions to create, update, and fetch a single channel

--- frontend/util/channels_api_util.js
+++ frontend/util/channels_api_util.js
@@ -4,3 +4,28 @@ export const fetchChannels = () => (
     url: '/api/channels'
   })
 );
+
+export const fetchChannel = (id) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/channels/${id}`
+  })
+);
+
+export const createChannel = (channel) => (
+  $.ajax({
+    method: 'POST',
+    url: `/api/channels`,
+    data: channel,
+    processData: false,
+    contentType: false
+  })
+);
+
+export const updateChannel = (channel) => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/channels/${channel.id}`,
+    data: {channel},
+  })
+);
