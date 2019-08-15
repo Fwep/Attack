87f0155ca87f09f3412738740b24341b7a1aaaa2
Taran Cacacho <tarancacacho@gmail.com>
2019-08-15 15:19:37 -0700

    Reformat for readability

--- frontend/components/channels/channel_list.jsx
+++ frontend/components/channels/channel_list.jsx
@@ -5,14 +5,23 @@ class ChannelList extends React.Component {
   render() {
     const { channels } = this.props;
     const channelItems = channels.map(channel => {
-      return <ChannelListItem
-        key={channel.id}
-        channel={channel} />
+      return (
+        <ChannelListItem
+          key={channel.id}
+          channel={channel} 
+        />
+      )
     });
 
     return (
       <div className="channel-list">
-        <h2>Channels</h2>
+        <div className="header-then-create-button">
+          <h2>Channels</h2>
+          <div className="tooltip">
+            <button onClick={() => this.props.openModal('createChannel')}>⊕</button>
+            <span className="tooltiptext">Create a channel</span>
+          </div>
+        </div>
         <ul>
           {channelItems}
         </ul>
