8b27611b88b6ed8b9d79ed2ce49c229b8af2d01d
Taran Cacacho <tarancacacho@gmail.com>
2019-08-15 15:18:34 -0700

    Redo Main Component component hierarchy

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import Modal from '../ui/modal';
 import AppNavBarContainer from '../nav/app-navbar_container';
 import SidebarContainer from '../sidebar/sidebar_container';
-import ChatRoomContainer from '../chat/chat_room_container';
+import MessagesContainer from '../messages/messages_container'
 
 class Main extends React.Component {
   render() {
     return (
-      <div id="main">
-        <AppNavBarContainer />
-        <SidebarContainer/>
-        <ChatRoomContainer />        
-      </div>
+      <>
+        <Modal />
+        <div id="main">
+          <AppNavBarContainer />
+          <SidebarContainer/>
+          {/* <ChatRoomContainer />         */}
+          <MessagesContainer />
+        </div>
+      </>
     );
   }
 }
