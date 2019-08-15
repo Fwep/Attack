9a4e31912ae0afff1662f9bd900c52d3ea06c453
Taran Cacacho <tarancacacho@gmail.com>
2019-08-15 15:16:41 -0700

    Remove devtools for push to production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+   applyMiddleware(thunk, logger)
   )
 );
 
