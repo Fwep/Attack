eb0c03e39252c167fe907f3974fdbe09cc9f1e39
Taran Cacacho <tarancacacho@gmail.com>
2019-08-15 15:20:55 -0700

    Give channel list container closeModal action

--- frontend/components/channels/channel_list_container.js
+++ frontend/components/channels/channel_list_container.js
@@ -2,13 +2,15 @@ import { connect } from 'react-redux';
 import ChannelList from './channel_list';
 import { getUsersChannels } from '../../reducers/selectors';
 import { fetchChannels } from '../../actions/channel_actions';
+import { openModal } from '../../actions/modal_actions';
 
 const mSP = state => ({
   channels: getUsersChannels(state)
 });
 
 const mDP = dispatch => ({
-  fetchChannels: () => dispatch(fetchChannels())
+  fetchChannels: () => dispatch(fetchChannels()),
+  openModal: (formType) => dispatch(openModal(formType))
 });
 
 export default connect(mSP, mDP)(ChannelList);
