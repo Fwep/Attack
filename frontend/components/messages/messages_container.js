25cf1c44e1d932e04b25cc3097623809fb1dc4b8
Taran Cacacho <tarancacacho@gmail.com>
2019-08-15 15:12:01 -0700

    Create container to deal with message rendering and form submission

---
+++ frontend/components/messages/messages_container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MessageListContainer from './message_list_container'
+import MessageFormContainer from './message_form_container';
+
+const MessagesContainer = () => (
+  <>
+    <MessageListContainer />
+    <MessageFormContainer />
+  </>
+);
+
+export default MessagesContainer;
