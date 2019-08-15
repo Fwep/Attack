0f13ee6d92dbfff54033448cb87b27a482a5c644
Taran Cacacho <tarancacacho@gmail.com>
2019-08-15 00:01:52 -0700

    Flesh out controller actions to render json errors

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,8 +1,12 @@
 class Api::ChannelsController < ApplicationController
   def show
     @messages = Message.all
-    # @channel = Channel.find(params[:id])
-    render "api/channels/show" if @channel
+    @channel = Channel.find_by(id: params[:id])
+    if @channel
+      render :show
+    else
+      render json: ["Channel not found"]
+    end
   end
 
   def index
@@ -14,23 +18,23 @@ class Api::ChannelsController < ApplicationController
     @channel = Channel.new(channel_params)
     @channel.creator_id = current_user.id
     if @channel.save
-      render "api/channels/show"
+      render :show
     else
-      render @channel.errors.full_messages
+      render json: @channel.errors.full_messages
     end
   end
 
   def update
     @channel = Channel.find(params[:id])
     if @channel.update(channel_params)
-      render "api/channels/show"
+      render :show
     else
-      render @channel.errors.full_messages
+      render json: @channel.errors.full_messages
     end
   end
 
   private
   def channel_params
-    params.require(:channel).permit(:name, :description, :private, :is_direct)
+    params.require(:channel).permit(:name, :description, :is_private, :is_direct)
   end
 end
