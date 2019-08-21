88a140a20d2d0fa9129911a872c44d8333419f4c
Taran Cacacho <tarancacacho@gmail.com>
2019-08-21 01:24:06 -0700

    Remove filler photoUrl, render based on message author

--- frontend/components/messages/message_list_item.jsx
+++ frontend/components/messages/message_list_item.jsx
@@ -33,11 +33,12 @@ class MessageListItem extends React.Component {
     
     const readableDate = `${day}, ${month} ${date}, ${hours}:${minutes} ${aside}`
 
+
     return (
       <li>
-        <img
-          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/461b1350-d5ec-4bdc-8278-d85cbc46e56a/dam9gnh-7a29b2f9-986a-473a-a44b-eead2b496a59.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ2MWIxMzUwLWQ1ZWMtNGJkYy04Mjc4LWQ4NWNiYzQ2ZTU2YVwvZGFtOWduaC03YTI5YjJmOS05ODZhLTQ3M2EtYTQ0Yi1lZWFkMmI0OTZhNTkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.V2yjqCqpSih9Nv5ZIwhPlhksrHcxoU76lCxXSAQaaqU" 
-        />
+          <img
+            src={message.photoUrl}
+          />
         <div className="message-data">
           <div className="message-header">
             <div className="authorname">{message.author}</div>
